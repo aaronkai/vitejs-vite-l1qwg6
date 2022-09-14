5bb77e00f920a9b233a2fa42530a3b435dcf067c
aaron <aaron.kai.hubbard@gmail.com>
2022-09-14 10:46:02 -0400

    add react-helmet and set header

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import WorkoutTable from "./components/WorkoutTable";
 import WeekPicker from "./components/WeekPicker";
 import TrainingMaxDisplay from "./components/TrainingMaxDisplay";
 import Footer from "./components/Footer";
+import { Helmet } from "react-helmet";
 
 function App() {
   const [cookies, setCookie] = useCookies(["oneRM", "TMsLogged"]);
@@ -58,12 +59,16 @@ function App() {
 
   return (
     <>
-      <meta name="description" content="workout helper for you" />
-      <link rel="icon" href="/favicon.ico" />
+      <Helmet>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="workout helper for you" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Workout Tracker</title>
+      </Helmet>
       <div className="grid bg-slate-200 min-h-screen content-between justify-center gap-4">
         <header>
           <Header title="Five Three One" />
-
           <Nav setFormVisible={setFormVisible} setCookie={setCookie} />
         </header>
         {formVisible && (
