0b0f7a215aac18fc02dc1ac8d530cdd0dbf11382
aaron <aaron.kai.hubbard@gmail.com>
2022-10-05 09:50:23 -0400

    move main workout code to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,14 @@
-import { useState, useEffect, cloneElement } from "react";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
-import OneRepMaxForm from "./components/OneRepMaxForm";
-import { useCookies } from "react-cookie";
-import WorkoutTable from "./components/WorkoutTable";
 import Footer from "./components/Footer";
 import { Helmet } from "react-helmet-async";
+import MainLifts from "./components/MainLifts";
+import { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
 
 function App() {
   const [cookies, setCookie] = useCookies(["oneRM", "oneRMLogged", "TM"]);
-  let squat, bench, deadlift, overhead;
-  let [squatTM, benchTM, deadliftTM, overheadTM] = [0, 0, 0, 0];
-  const [week, setWeek] = useState(0);
   const [formVisible, setFormVisible] = useState(true);
-
-  if (cookies.oneRM) {
-    ({ squat, bench, deadlift, overhead } = cookies.oneRM);
-    if (!cookies.TM) {
-      [squatTM, benchTM, deadliftTM, overheadTM] = [
-        squat * 0.9,
-        bench * 0.9,
-        deadlift * 0.9,
-        overhead * 0.9,
-      ];
-    }
-  }
-
-  const [oneRM, setOneRM] = useState({
-    squat,
-    bench,
-    deadlift,
-    overhead,
-  });
-
-  if (cookies.TM) {
-    [squatTM, benchTM, deadliftTM, overheadTM] = [
-      cookies.TM.squat,
-      cookies.TM.bench,
-      cookies.TM.deadlift,
-      cookies.TM.overhead,
-    ];
-  }
-
-  const [TM, setTM] = useState({
-    squat: squatTM,
-    bench: benchTM,
-    deadlift: deadliftTM,
-    overhead: overheadTM,
-  });
-
-  useEffect(() => {
-    if (
-      oneRM.squat > 0 ||
-      oneRM.deadlift > 0 ||
-      oneRM.overhead > 0 ||
-      oneRM.bench > 0
-    ) {
-      console.log("setting oneRM cookie in oneRM use effect");
-      setCookie("oneRM", oneRM, { path: "/" });
-    }
-  }, [oneRM]);
-
-  useEffect(() => {
-    if (TM.squat > 0 || TM.deadlift > 0 || TM.overhead > 0 || TM.bench > 0) {
-      console.log("setting TM cookie in TM use effect");
-      setCookie("TM", TM, { path: "/" });
-    }
-  }, [TM]);
-
-  useEffect(() => {
-    console.log({ formVisible });
-    console.log("cookie one rep max " + cookies.oneRMLogged);
-    if (cookies.oneRMLogged == "true") {
-      console.log("1RM set. set form invisible");
-      setFormVisible(false);
-    } else {
-      console.log("no 1RM set. make form visible");
-      setFormVisible(true);
-    }
-  }, [cookies.oneRMLogged]);
 
   return (
     <>
@@ -103,31 +33,13 @@ function App() {
             <Header title="5/3/1" />
             <Nav setFormVisible={setFormVisible} setCookie={setCookie} />
           </header>
-          <div className=" w-full ">
-            {formVisible && (
-              <div className=" max-w-2xl mx-auto ">
-                <OneRepMaxForm
-                  oneRM={oneRM}
-                  setOneRM={setOneRM}
-                  TM={TM}
-                  setTM={setTM}
-                  setFormVisible={setFormVisible}
-                  setCookie={setCookie}
-                />
-              </div>
-            )}
-            {!formVisible && (
-              <div className="grid grid-row-3 content-start p-4 max-w-2xl mx-auto">
-                <WorkoutTable
-                  week={week}
-                  setWeek={setWeek}
-                  TM={TM}
-                  setTM={setTM}
-                  setFormVisible={setFormVisible}
-                />
-              </div>
-            )}
-          </div>
+          <MainLifts
+            setFormVisible={setFormVisible}
+            setCookie={setCookie}
+            cookies={cookies}
+            formVisible={formVisible}
+          />
+
           <Footer />
         </div>
       </div>
---
+++ src/components/MainLifts.tsx
@@ -0,0 +1,122 @@
+import OneRepMaxForm from "./OneRepMaxForm";
+import WorkoutTable from "./WorkoutTable";
+import { useState, useEffect } from "react";
+
+interface Props {
+  setFormVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  formVisible: boolean;
+  setCookie: any;
+  cookies: {
+    oneRM?: any;
+    TM?: any;
+    oneRMLogged?: any;
+  };
+}
+
+export default function MainLifts({
+  cookies,
+  setCookie,
+  formVisible,
+  setFormVisible,
+}: Props) {
+  // const [cookies, setCookie] = useCookies(["oneRM", "oneRMLogged", "TM"]);
+  let squat, bench, deadlift, overhead;
+  let [squatTM, benchTM, deadliftTM, overheadTM] = [0, 0, 0, 0];
+  const [week, setWeek] = useState(0);
+  // const [formVisible, setFormVisible] = useState(true);
+
+  if (cookies.oneRM) {
+    ({ squat, bench, deadlift, overhead } = cookies.oneRM);
+    if (!cookies.TM) {
+      [squatTM, benchTM, deadliftTM, overheadTM] = [
+        squat * 0.9,
+        bench * 0.9,
+        deadlift * 0.9,
+        overhead * 0.9,
+      ];
+    }
+  }
+
+  const [oneRM, setOneRM] = useState({
+    squat,
+    bench,
+    deadlift,
+    overhead,
+  });
+
+  if (cookies.TM) {
+    [squatTM, benchTM, deadliftTM, overheadTM] = [
+      cookies.TM.squat,
+      cookies.TM.bench,
+      cookies.TM.deadlift,
+      cookies.TM.overhead,
+    ];
+  }
+
+  const [TM, setTM] = useState({
+    squat: squatTM,
+    bench: benchTM,
+    deadlift: deadliftTM,
+    overhead: overheadTM,
+  });
+
+  useEffect(() => {
+    if (
+      oneRM.squat > 0 ||
+      oneRM.deadlift > 0 ||
+      oneRM.overhead > 0 ||
+      oneRM.bench > 0
+    ) {
+      console.log("setting oneRM cookie in oneRM use effect");
+      setCookie("oneRM", oneRM, { path: "/" });
+    }
+  }, [oneRM]);
+
+  useEffect(() => {
+    if (TM.squat > 0 || TM.deadlift > 0 || TM.overhead > 0 || TM.bench > 0) {
+      console.log("setting TM cookie in TM use effect");
+      setCookie("TM", TM, { path: "/" });
+    }
+  }, [TM]);
+
+  useEffect(() => {
+    console.log({ formVisible });
+    console.log("cookie one rep max " + cookies.oneRMLogged);
+    if (cookies.oneRMLogged == "true") {
+      console.log("1RM set. set form invisible");
+      setFormVisible(false);
+    } else {
+      console.log("no 1RM set. make form visible");
+      setFormVisible(true);
+    }
+  }, [cookies.oneRMLogged]);
+  return (
+    <>
+      <div className=" w-full ">
+        {formVisible && (
+          <div className=" max-w-2xl mx-auto ">
+            <OneRepMaxForm
+              oneRM={oneRM}
+              setOneRM={setOneRM}
+              TM={TM}
+              setTM={setTM}
+              setFormVisible={setFormVisible}
+              setCookie={setCookie}
+            />
+          </div>
+        )}
+        {!formVisible && (
+          <div className="grid grid-row-3 content-start p-4 max-w-2xl mx-auto">
+            <WorkoutTable
+              week={week}
+              setWeek={setWeek}
+              TM={TM}
+              setTM={setTM}
+              setFormVisible={setFormVisible}
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
